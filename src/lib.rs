4c4ee0614c47dbacadcc7c8546595c52e6881c63
Kjetil Kjeka <kjetilkjeka@gmail.com>
2018-12-10 19:13:34 +0100

    Update to newer version of racr to get field variants and rename use

--- src/lib.rs
+++ src/lib.rs
@@ -63,28 +63,47 @@
 //!     racr_parser::UseParser::new().parse("use foo::bar::Baz;").unwrap()
 //! );
 //! ```
+//! ```
+//! assert_eq!(
+//!     racr::Use{tree: racr::UseTree::Path{path_segment: "foo".into(), sub_tree:
+//!         Box::new( racr::UseTree::Rename{ident: "Bar".into(), rename: "Baz".into()} )
+//!     }},
+//!     racr_parser::UseParser::new().parse("use foo::Bar as Baz;").unwrap()
+//! );
+//! ```
 //! ## Parse register definitions
 //! ```
 //! assert_eq!(
 //!     racr::RegisterDefinition{
 //!         access: racr::Access::WriteOnly,
 //!         ident: "Foo".into(),
-//!         description: Some(String::from("Some description")),
+//!         documentation: Some(String::from("Some documentation")),
 //!         size: 32,
 //!         reset_value: Some(0x00),
 //!         fields: vec![
-//!             racr::FieldInstance{ident: "bar".into(), description: None, bit_start: 0, bit_end: 7, access: None},
-//!             racr::FieldInstance{ident: "baz".into(), description: None, bit_start: 8, bit_end: 8, access: Some(racr::Access::ReadOnly)},
-//!             racr::FieldInstance{ident: "bax".into(), description: Some(String::from("Some description")), bit_start: 9, bit_end: 31, access: None},
+//!             racr::FieldInstance{ident: "bar".into(), documentation: None, bit_range: 0..4, access: None, variants: Vec::new()},
+//!             racr::FieldInstance{ident: "barX".into(), documentation: None, bit_range: 4..8, access: None, variants: vec![
+//!                 racr::FieldVariant{ident: "BarA".into(), value: 0, documentation: None},
+//!                 racr::FieldVariant{ident: "BarB".into(), value: 2, documentation: Some(String::from("some documentation"))},
+//!                 racr::FieldVariant{ident: "BarC".into(), value: 4, documentation: None},
+//!             ]},
+//!             racr::FieldInstance{ident: "baz".into(), documentation: None, bit_range: 8..9, access: Some(racr::Access::ReadOnly), variants: Vec::new()},
+//!             racr::FieldInstance{ident: "bax".into(), documentation: Some(String::from("Some documentation")), bit_range: 9..32, access: None, variants: Vec::new()},
 //!         ],
 //!     },
 //!     racr_parser::RegisterDefinitionParser::new().parse(" 
-//! #[doc = \"Some description\"]
+//! #[doc = \"Some documentation\"]
 //! WriteOnly register[32] Foo = 0 {
-//!    bar[0..7],
-//!    ReadOnly baz[8],
-//!    #[doc = \"Some description\"]
-//!    bax[9..31],
+//!     bar[0..4],
+//!     barX[4..8] {
+//!         BarA = 0,
+//!         #[doc = \"some documentation\"]
+//!         BarB = 0b10,
+//!         BarC = 0x4,
+//!     },
+//!     ReadOnly baz[8],
+//!     #[doc = \"Some documentation\"]
+//!     bax[9..32],
 //! }"
 //!     ).unwrap()
 //! );
@@ -94,7 +113,7 @@
 //! assert_eq!(
 //!     racr::PeripheralDefinition{
 //!         ident: "Foo".into(),
-//!         description: Some(String::from("Some description")),
+//!         documentation: Some(String::from("Some documentation")),
 //!         registers: vec![
 //!             racr::RegisterSlot::Single{instance: racr::RegisterInstance{ident: "bar".into(), ty: racr::RegisterType::Single{path: racr_parser::PathParser::new().parse("bar::Bar").unwrap()}}, offset: 0x0},
 //!             racr::RegisterSlot::Single{instance: racr::RegisterInstance{ident: "bax".into(), ty: racr::RegisterType::Array{path: racr_parser::PathParser::new().parse("bax::Bax").unwrap(), size: 2}}, offset: 0x4},
@@ -109,7 +128,7 @@
 //!         ],
 //!     },
 //!     racr_parser::PeripheralDefinitionParser::new().parse(" 
-//! #[doc = \"Some description\"]
+//! #[doc = \"Some documentation\"]
 //! peripheral Foo {
 //!    bar: bar::Bar @ 0x00,
 //!    bax: [bax::Bax; 2] @ 0x04,
@@ -123,7 +142,7 @@
 //! assert_eq!(
 //!     racr::DeviceDefinition{
 //!         ident: "Foo".into(),
-//!         description: Some(String::from("Some description")),
+//!         documentation: Some(String::from("Some documentation")),
 //!         peripherals: vec![
 //!             racr::PeripheralInstance{ident: "bar".into(), path: racr_parser::PathParser::new().parse("bar::Bar").unwrap(), address: 0x0},
 //!             racr::PeripheralInstance{ident: "baz".into(), path: racr_parser::PathParser::new().parse("baz::Baz").unwrap(), address: 0x4},
@@ -131,7 +150,7 @@
 //!         ],
 //!     },
 //!     racr_parser::DeviceDefinitionParser::new().parse(" 
-//! #[doc = \"Some description\"]
+//! #[doc = \"Some documentation\"]
 //! device Foo {
 //!    bar: bar::Bar @ 0x00,
 //!    baz: baz::Baz @ 0x04,
